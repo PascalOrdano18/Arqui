3532225399357540736241fdd2f2d7b5a59dbd60
PascalOrdano18 <pascalordanodev@gmail.com>
2025-12-11 15:59:18 -0300

    bien hecho con orden push y pop

--- Guias/Guia2/ej3v2.asm
+++ Guias/Guia2/ej3v2.asm
@@ -22,6 +22,15 @@ _start:
 
 	add esp, 8  ; con esto limpio el stack frame de la funcion num2str
 
+	;; prueba a ver si se armo bien el string
+
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, buffer
+	mov edx, 20
+	int 80h
+
+
 	mov eax, 1
 	mov ebx, 0
 	int 80h
@@ -35,10 +44,12 @@ num2str:
 	mov edi, [ebp + 12] ;    edi es el puntero donde debe quedar el string
 	mov esi, edi
 
+	mov ecx, 0
+
 
 loop:
 	cmp eax, 0  ; eax es el cociente, si es cero, ya no divido mas
-	je end
+	je write
 
 	; me preparo para dividir
 	; el resto queda en edx, que va a ser el numero digito a pasar a string
@@ -49,15 +60,22 @@ loop:
 	; uso dl porque es la parte baja de edx, son 8 bits, un byte, lo que ocupa mi char.
 	; esto me sirve para poner en el lugar del array ese byte exacto, volviendo la operacion legal,; byte a byte
 	add dl, '0'  ; le sumo el ascii del cero, ahora converti el numero a su ascii
-	inc esi
-
-	mov byte [esi], dl
+	push edx
+	inc ecx
 	jmp loop
 	
 
+write:
+	pop eax
+	mov [esi], eax
+	inc esi
+	dec ecx
+	cmp ecx, 0 
+	jne write
+
 
 end:
-	
+	mov byte [esi + 1], 0
 	mov esp, ebp
 	pop ebp
 
